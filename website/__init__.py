e09da3ffcab2828199bf7719beac10a61f9470d1
paczek224 <paczeklukasz90@gmail.com>
2022-07-09 18:33:20 +0200

    added user and notes model

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
 
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 def create_app():
     app = Flask(__name__)
     app.config['SESSION_TYPE'] = 'filesystem'
-    app.secret_key = ''
-    app.config['SECRET'] = ''
+    app.config['SECRET'] = 'dsadsad'
+    app.secret_key = 'dsadsad'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -13,4 +21,13 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database')
--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+
+from website import db
+from website.models import User
+from werkzeug.security import  generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=["GET", 'POST'])
 def login():
-    data = request.form
-    print(data)
     return render_template("login.html", user="None")
 
 
@@ -25,13 +27,18 @@ def sing_up():
 
         if (len(email) < 4):
             flash('Email must be greater than 4 characters', category='error')
-        elif len(firstName) < 5:
+        elif len(firstName) < 2:
             flash('firstName must be greater than 2 characters', category='error')
         elif len(password1) < 2:
             flash('password1 must be greater than 2 characters', category='error')
         elif len(password2) < 2:
             flash('password2 must be greater than 2 characters', category='error')
         else:
-            flash('Account created', category='success')
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password1, 'sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash(f'Account created email{new_user.email}, name {new_user.first_name}', category='success')
+            return redirect(url_for("views.home"))
 
     return render_template("sign_up.html")
